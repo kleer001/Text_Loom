f7c096234180ec53e2b3d12c4098b9dc6703c1d7
kleer001 <3crows@gmail.com>
2024-10-10 15:43:54 -0700

    continue work not broken but not working

--- src/backend/testtextglobals_1.py
+++ src/backend/testtextglobals_1.py
@@ -4,15 +4,17 @@ from print_node_info import print_node_info
 from nodes.looper_node import *
 from global_store import GlobalStore
 
+globals = GlobalStore()
+
 #set global variables 
-
-
-
+globals.set("AB", 6)
+globals.set("FOO", "apples")
+globals.set("BAR", 69)
 # Create nodes
 text1 = Node.create_node(NodeType.TEXT, node_name="text1")
 
 # Set the parameters for text nodes
-text1._parms["text_string"].set("Filler Text 1")
+text1._parms["text_string"].set("Filler Text ${$FOO and $BAR}")
 # Connect nodes
 #text1.set_input(0, text2)
 
@@ -20,9 +22,10 @@ text1._parms["text_string"].set("Filler Text 1")
 #print_node_info(text2)
 
 print(":: EVAL ::")
-texteval2 = text1.eval()
+texteval = text1.cook()
 print_node_info(text1)
-print("text1 evals to: \n",texteval2)
+evaltext = text1._parms["text_string"].eval()
+print("text1 evals to: \n",evaltext)
 
 
 # text1._parms["pass_through"].set(False)
