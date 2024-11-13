7cf2cdebea965c0f7a982bdbff93428b9855a6f5
kleer001 <3crows@gmail.com>
2024-11-12 22:07:59 -0800

    just barely working

---
+++ src/TUI/help_tui.md
@@ -0,0 +1,61 @@
+[NODE]
+Node Mode Commands:
+- h/j/k/l: Navigate nodes
+- Enter: Select node
+- Space: Expand/collapse
+- r: Refresh node tree
+
+[PARAMETER]
+Parameter Mode Commands:
+- j/k: Navigate parameters
+- Enter: Edit parameter
+- Esc: Exit edit mode
+- Tab: Next field
+
+[GLOBAL]
+Global Mode Commands:
+- j/k: Navigate variables
+- Enter: Edit value
+- d: Delete variable
+- n: New variable
+
+[FILE]
+File Mode Commands:
+- j/k: Navigate files
+- Enter: Select
+- q: Exit to previous mode
+- s: Save
+- S: Save As
+
+[HELP]
+Help Mode
+This IS the help system. Press Ctrl+h in any mode to see relevant commands.
+Use Ctrl + (key) to switch modes:
+n: Node    p: Parameter    g: Global
+f: File    h: Help        k: Keymap
+t: Status  o: Output
+
+[KEYMAP]
+Keymap Mode Commands:
+- j/k: Navigate bindings
+- Enter: Edit binding
+- d: Delete binding
+- n: New binding
+- q: Exit to previous mode
+
+[STATUS]
+Status Mode Commands:
+- j/k: Scroll output
+- G: Jump to bottom
+- gg: Jump to top
+- /: Search
+- n: Next match
+- N: Previous match
+
+[OUTPUT]
+Output Mode Commands:
+- j/k: Scroll output
+- G: Jump to bottom
+- gg: Jump to top
+- c: Clear output
+- /: Search
--- src/TUI/palette.py
+++ src/TUI/palette.py
@@ -1,112 +1,24 @@
 from textual.color import Color
 
-class TUIPalette:
-    BACKGROUND_BASE = "#1c1c1c"
-    BACKGROUND_LIGHT = "#303030"
-    FOREGROUND = "#d7d7d7"
-    HIGHLIGHT = "#444444"
-    BLUE = "#5f87af"
-    WHITE = "#d7d7d7"
-    BLACK = "#121212"
-    GRAY = "#444444"
-
-STYLES = {
-    "app": {
-        "background": TUIPalette.BLACK,
-        "border_title_align": "left",
-    },
+class TUIColors:
+    WHITE = Color.parse("white")
+    BLACK = Color.parse("black")
+    BLUE = Color.parse("blue")
+    LIGHT_GRAY = Color.parse("#f8f8f2")
+    DARK_GRAY = Color.parse("#303030")
+    HIGHLIGHT = Color.parse("#44475a")
     
-    "Window": {
-        "border": f"round {TUIPalette.GRAY}",
-        "background": TUIPalette.BACKGROUND_BASE,
-    },
+    MODELINE_FG = WHITE
+    MODELINE_BG = BLUE
     
-    "Window.active": {
-        "border": f"round {TUIPalette.BLUE}",
-        "background": TUIPalette.BACKGROUND_LIGHT,
-    },
+    ACTIVE_WINDOW_FG = BLACK
+    ACTIVE_WINDOW_BG = Color.parse("#e6e6e6")  # Equivalent to 230
     
-    "ModeLine": {
-        "dock": "bottom",
-        "height": 1,
-        "background": TUIPalette.BLUE,
-        "color": TUIPalette.WHITE,
-        "padding": (0, 1),
-    },
+    INACTIVE_WINDOW_FG = Color.parse("#a8a8a8")  # Equivalent to 248
+    INACTIVE_WINDOW_BG = Color.parse("#444444")  # Equivalent to 238
     
-    "HelpMode": {
-        "dock": "bottom",
-        "height": "12%",
-        "border": f"round {TUIPalette.GRAY}",
-        "background": TUIPalette.BACKGROUND_BASE,
-        "color": TUIPalette.FOREGROUND,
-        "padding": (0, 1),
-    },
+    CURSOR_FG = Color.parse("#f7f7f7")  # Equivalent to 231
+    CURSOR_BG = BLACK
     
-    "NodeMode": {
-        "width": "20%",
-        "background": TUIPalette.BACKGROUND_BASE,
-        "color": TUIPalette.FOREGROUND,
-        "border": f"round {TUIPalette.GRAY}",
-    },
-    
-    "ParameterMode": {
-        "dock": "right",
-        "width": "40%",
-        "background": TUIPalette.BACKGROUND_BASE,
-        "color": TUIPalette.FOREGROUND,
-        "border": f"round {TUIPalette.GRAY}",
-    },
-    
-    "StatusMode": {
-        "height": "50%",
-        "background": TUIPalette.BACKGROUND_BASE,
-        "color": TUIPalette.FOREGROUND,
-        "border": f"round {TUIPalette.GRAY}",
-    },
-    
-    "OutputMode": {
-        "height": "25%",
-        "background": TUIPalette.BACKGROUND_BASE,
-        "color": TUIPalette.FOREGROUND,
-        "border": f"round {TUIPalette.GRAY}",
-    },
-    
-    "GlobalMode": {
-        "height": "25%",
-        "background": TUIPalette.BACKGROUND_BASE,
-        "color": TUIPalette.FOREGROUND,
-        "border": f"round {TUIPalette.GRAY}",
-    },
-    
-    "FileMode": {
-        "background": TUIPalette.BACKGROUND_BASE,
-        "color": TUIPalette.FOREGROUND,
-        "border": f"round {TUIPalette.GRAY}",
-    },
-    
-    "KeymapMode": {
-        "background": TUIPalette.BACKGROUND_BASE,
-        "color": TUIPalette.FOREGROUND,
-        "border": f"round {TUIPalette.GRAY}",
-    },
-    
-    "ParameterWidget": {
-        "height": "auto",
-        "background": TUIPalette.BACKGROUND_BASE,
-        "color": TUIPalette.FOREGROUND,
-        "border": f"round {TUIPalette.GRAY}",
-        "margin": (0, 0, 1, 0),
-    },
-    
-    "ParameterWidget.active": {
-        "border": f"round {TUIPalette.BLUE}",
-        "background": TUIPalette.BACKGROUND_LIGHT,
-    },
-    
-    "ScrollableContainer": {
-        "height": "100%",
-        "border": f"round {TUIPalette.GRAY}",
-        "background": TUIPalette.BACKGROUND_BASE,
-    }
-}
+    GUTTER_FG = BLACK
+    GUTTER_BG = BLACK
